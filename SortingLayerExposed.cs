ef961bb06e0fbc7e0a68699b732dd59b3273cd43
Nick Gravelyn <nick@gravelyn.com>
2016-03-19 10:11:02 -0700

    Initial copy from UnityToolbag

---
+++ SortingLayerExposed.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SortingLayerTools
+{
+    // Component does nothing; editor script does all the magic
+    [AddComponentMenu("SortingLayerTools/Sorting Layer Exposed")]
+    public class SortingLayerExposed : MonoBehaviour
+    {
+    }
+}
